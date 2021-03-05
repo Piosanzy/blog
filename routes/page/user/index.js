de232eb74fecc0166416760a7435f6dff35e799b
이인택 <hotdog4750323@naver.com>
2021-03-05 17:32:27 +0900

    어드민 페이지 작성

--- routes/page/user/index.js
+++ routes/page/user/index.js
@@ -6,9 +6,9 @@ const authMiddleware = require('../../../middlewares/auth/index');
 /* GET home page. */
 router.get('/mypage', authMiddleware, function (req, res, next) {
     if (req.decoded.admin) {
-        res.render('page/user/adminpage',res.decoded);
+        res.render('page/user/adminpage',req.decoded);
     } else {
-        res.render('page/user/mypage',res.decoded);
+        res.render('page/user/mypage',req.decoded);
     }
 });
 
